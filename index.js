f1f13b10ddbcaf34f6b9a8a4b79df67c0ee9fffb
ahmed-jobayer <jobayer320721@.com>
2024-05-16 20:15:18 +0600

    server setup compleated

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('Server of Electro Fix is running')
+})
+
+app.listen(port, () => {
+    console.log(`Server of Electro Fix is running on port ${port}`)
+})
+
+// n28Wq2CjHIFrfGam
+// ElectroFix
